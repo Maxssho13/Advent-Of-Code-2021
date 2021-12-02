ae9b4a0ceae5c27afc0441dc9c9f5f12b3901cfa
Max Glass <glassmax13@gmail.com>
2021-12-02 07:27:18 -0500

    Day 1: optimize part 2

--- src/bin/01_part_2.rs
+++ src/bin/01_part_2.rs
@@ -7,15 +7,11 @@ fn main() {
         .map(|line| line.parse::<i32>().unwrap())
         .collect();
 
-    let mut prev_sum = input[0] + input[1] + input[2];
-
-    for i in 1..input.len() - 2 {
-        let new_sum = input[i] + input[i + 1] + input[i + 2];
-        if new_sum > prev_sum {
-            increases += 1;
+    input.iter().zip(input.iter().skip(3)).for_each(|(a, b)| {
+        if b > a {
+            increases += 1
         }
-        prev_sum = new_sum;
-    }
+    });
 
     println!("output: {}", increases);
 }
